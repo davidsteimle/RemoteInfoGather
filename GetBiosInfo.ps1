31000d9a5567b172e14d580662e64f5e178f955f
David Steimle <davidsteimle@gmail.com>
2020-04-16 09:04:49 -0400

    Update GetBiosInfo.ps1

--- GetBiosInfo.ps1
+++ GetBiosInfo.ps1
@@ -1,12 +1,27 @@
-$obj = New-Object -Type PSObject | Select-Object SerialNumber, Manufacturer, UUID, BaseBoardProduct, ChassisTypes, Chassis, SystemFamily, SystemSKUNumber
+<#
+Partially taken from: https://docs.microsoft.com/en-us/windows-hardware/drivers/bringup/sample-powershell-script-to-query-smbios-locally
+#>
 
-$obj.SerialNumber = Get-WmiObject -class Win32_Bios -namespace $namespace | Select-Object -ExpandProperty SerialNumber
-$obj.Manufacturer = Get-WmiObject -class Win32_Bios -namespace $namespace | Select-Object -ExpandProperty Manufacturer
-$obj.UUID = Get-WmiObject Win32_ComputerSystemProduct | Select-Object -ExpandProperty UUID
-$obj.BaseBoardProduct = Get-WmiObject Win32_BaseBoard | Select-Object -ExpandProperty Product
-$obj.ChassisTypes = Get-WmiObject Win32_SystemEnclosure | Select-Object -ExpandProperty ChassisTypes
-$obj.Chassis = $ChassisTypes[[int]$obj.ChassisTypes]
-$obj.SystemFamily = Get-WmiObject Win32_ComputerSystem | Select-Object -ExpandProperty SystemFamily
-$obj.SystemSKUNumber = Get-WmiObject Win32_ComputerSystem | Select-Object -ExpandProperty SystemSKUNumber
+$namespace = "root\CIMV2"
 
-Get-WmiObject -Class Win32_Bios
+$ScriptBlock1 = {
+  $obj1 = New-Object -Type PSObject | Select-Object SerialNumber, Manufacturer, UUID, BaseBoardProduct, ChassisTypes, Chassis, SystemFamily, SystemSKUNumber
+
+  $obj1.SerialNumber = Get-WmiObject -class Win32_Bios -namespace $namespace | Select-Object -ExpandProperty SerialNumber
+  $obj1.Manufacturer = Get-WmiObject -class Win32_Bios -namespace $namespace | Select-Object -ExpandProperty Manufacturer
+  $obj1.UUID = Get-WmiObject Win32_ComputerSystemProduct | Select-Object -ExpandProperty UUID
+  $obj1.BaseBoardProduct = Get-WmiObject Win32_BaseBoard | Select-Object -ExpandProperty Product
+  $obj1.ChassisTypes = Get-WmiObject Win32_SystemEnclosure | Select-Object -ExpandProperty ChassisTypes
+  $obj1.SystemFamily = Get-WmiObject Win32_ComputerSystem | Select-Object -ExpandProperty SystemFamily
+  $obj1.SystemSKUNumber = Get-WmiObject Win32_ComputerSystem | Select-Object -ExpandProperty SystemSKUNumber
+  
+  $obj1
+}
+
+$ScriptBlock2 = {
+  Get-WmiObject -class Win32_Bios -namespace $namespace
+  Get-WmiObject Win32_ComputerSystemProduct
+  Get-WmiObject Win32_BaseBoard
+  Get-WmiObject Win32_SystemEnclosure
+  Get-WmiObject Win32_ComputerSystem
+}
